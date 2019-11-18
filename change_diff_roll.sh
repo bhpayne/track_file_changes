44f958b22fdded9eb9f071a28906447eef2f837c
Ben Payne <ben.is.located@gmail.com>
2019-11-18 13:40:11 -0500

    sh rather than py

---
+++ change_diff_roll.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+path_to_json="."
+
+./change_tracker.py --search_path="./tmp" --write_path=${path_to_json} --output_prefix="logs"
+
+python diff_changes.py --path_to_json=${path_to_json}  --path_to_output="."
+
+rolling_logs.py --path_to_json=${path_to_json}  --number_to_keep="3"
+
+# email here
+
